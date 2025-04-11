3ba480c98546ef9d34ac2bef5249531a80a06336
Aryan Sharma <aryan.shr.04@gmail.com>
2025-04-12 02:13:58 +0530

    Add: Completion of basic implementation

--- app.py
+++ app.py
@@ -6,4 +6,24 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['GET','POST'])
 def index():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return render_template('index.html', error = 'No file part')
+        file = request.files['file']
+        if file.filename == '':
+            return render_template('index.html', error = 'No file selected')
+        if file:
+            file_path = 'uploaded_file.txt'
+            file.save(file_path)
+            stop_words = ["is","a","the","and","are","about","it","lets"]
+            tokens, frequency = tokeniser.process_file(file_path, stop_words)
+            os.remove(file_path)
+            return render_template('result.html', tokens=tokens, frequency = frequency)
+        return render_template('index.html')
     
+@app.route('/result')
+def results():
+    return render_template('result.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
